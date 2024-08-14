17a430a34dc2531900e8a63bcdb5ec20301af1a7
sirilgsq <siril.m@geesesquads.com>
2024-08-14 12:38:19 +0530

    json updated

--- config/firebase-admin.js
+++ config/firebase-admin.js
@@ -1,28 +1,16 @@
 const admin = require("firebase-admin");
 const fs = require("fs");
 
-const serviceAccountJson = () => {
-  const serviceAccountPath = process.env.FB_ADMIN_SDK_JSON_PATH;
-  if (!serviceAccountPath) {
-    throw new Error("FB_ADMIN_SDK_JSON_PATH environment variable is not set.");
-  }
-  fs.readFile(serviceAccountPath, "utf8", (err, data) => {
-    if (err) {
-      throw new Error(`Error reading the JSON file: ${err}`);
-    }
-
-    try {
-      return JSON.parse(data);
-    } catch (parseError) {
-      throw new Error(`Error parsing the JSON file: ${parseError}`);
-    }
-  });
-};
-
-const serviceAccount = serviceAccountJson();
+const serviceAccountPath = process.env.FB_ADMIN_SDK_JSON_PATH;
+if (!serviceAccountPath) {
+  throw new Error("FB_ADMIN_SDK_JSON_PATH environment variable is not set.");
+}
+const serviceAccountJson = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, serviceAccountPath), "utf8")
+);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccountJson),
   databaseURL: "https://decotherapynode-default-rtdb.firebaseio.com/",
 });
 const fdb = admin.firestore();
