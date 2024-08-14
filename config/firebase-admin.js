613e668902f0c87f5491ff536a154d89b9ff3e19
sirilgsq <siril.m@geesesquads.com>
2024-08-14 12:31:32 +0530

    service account json updated

--- config/firebase-admin.js
+++ config/firebase-admin.js
@@ -1,12 +1,8 @@
 const admin = require("firebase-admin");
-
-const serviceAccountString = process.env.FB_ADMIN_SDK_JSON_PATH;
-const serviceAccount = JSON.parse(
-  Buffer.from(serviceAccountString, "base64").toString("utf8")
-);
+const fs = require("fs");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccountJson()),
   databaseURL: "https://decotherapynode-default-rtdb.firebaseio.com/",
 });
 const fdb = admin.firestore();
@@ -16,4 +12,22 @@ module.exports = {
   admin,
   fdb,
   rdb,
+};
+
+const serviceAccountJson = () => {
+  const serviceAccountPath = process.env.FB_ADMIN_SDK_JSON_PATH;
+  if (!serviceAccountPath) {
+    throw new Error("FB_ADMIN_SDK_JSON_PATH environment variable is not set.");
+  }
+  fs.readFile(serviceAccountPath, "utf8", (err, data) => {
+    if (err) {
+      throw new Error(`Error reading the JSON file: ${err}`);
+    }
+
+    try {
+      return JSON.parse(data);
+    } catch (parseError) {
+      throw new Error(`Error parsing the JSON file: ${parseError}`);
+    }
+  });
 };
